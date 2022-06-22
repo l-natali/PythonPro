e373b1a6e8aa0a90e8fb97e3c6ebe7e0360062db
l-natali <lukienko.natasha@gmail.com>
2022-06-22 23:00:42 +0300

    lesson 2

--- group.py
+++ group.py
@@ -1,41 +1,57 @@
-from student import Student, student_1, student_2, student_3, student_4, \
-    student_5, student_6, student_7, student_8, student_9, student_10
+from students import Students, st1, st2, st3, st4, st5, st6, st7, st8, st9, st10
 
 
 class Group:
-    def __init__(self, student: Student):
+    def __init__(self, student: Students):
         self.student = student
-        self.group_st = []
+        self.group = []
 
     def __str__(self):
-        return f'{self.student.surname}, {self.group_st}'
+        res = f'{self.student}'
+        for i in self.group:
+            tmp = f'{i}\n'
+            res += tmp
+        return res
 
-    def add_student(self, student: Student):
-        self.group_st.append(student)
+    def add_student(self, student: Students):
+        self.group.append(student)
         return
 
-    def get_students(self):
-        return self.group_st
+    def del_student(self, student: Students):
+        self.group.remove(student)
+        return
 
-    def del_students(self, student: Student):
-        self.group_st.remove(student)
-
-    def find_student(self, student: Student):
-        if student.surname in self.group_st:
-            return 'yes'
+    def find_student(self, student: Students):
+        if student.surname in self.group:
+            return 'Yes'
         else:
-            return 'no'
+            return 'No'
+
+    def get_group(self):
+        return self.group
 
     def count_students(self):
-        if len(self.group_st) > 10:
+        res = 0
+        for i in self.group:
+            res += 1
+        if res > 10:
             return 'error'
+        return res
+
+# student_find =
+group = Group(st1)
+group.add_student(st1)
+group.add_student(st2)
+group.add_student(st3)
+group.add_student(st4)
+group.add_student(st5)
+group.add_student(st6)
+group.add_student(st7)
+group.add_student(st8)
+group.add_student(st9)
+group.add_student(st10)
+group.del_student(st8)
+group.del_student(st4)
 
 
-group = Group(student_1)
-group.add_student(student_2.surname)
-group.add_student(student_7.surname)
-group.add_student(student_5.surname)
-group.add_student(student_8.surname)
-group.del_students(student_2.surname)
-
-print(group.get_students(), group.find_student(student_6), group.count_students(), sep='\n')
+print(group, 'Count students - ', group.count_students(), '\n', group.find_student(st2))
---
+++ person.py
@@ -0,0 +1,8 @@
+class Person:
+    def __init__(self, name, surname, sex):
+        self.name = name
+        self.surname = surname
+        self.sex = sex
+
+    def __str__(self):
+        return f'{self.name} {self.surname}, {self.sex}'
