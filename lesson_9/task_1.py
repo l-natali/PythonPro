7f32d241c03b8ed3a56319f2449e8ae9e882859f
l-natali <lukienko.natasha@gmail.com>
2022-07-17 21:21:33 +0300

    lesson 9

---
+++ lesson_9/task_1.py
@@ -0,0 +1,17 @@
+def count_func(f):
+    def counter(*args):
+        counter.count += 1
+        return f(*args)
+    counter.count = 0
+    return counter
+
+
+@count_func
+def summa(x, y):
+    return x + y
+
+
+print(summa(2, 3))
+print(summa(3, 4))
+print(summa(4, 5))
+print('Function was called', summa.count, 'times')
