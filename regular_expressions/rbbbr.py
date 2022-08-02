865768aaaf3b026a8cc77c4224e8236dc20f9059
l-natali <lukienko.natasha@gmail.com>
2022-08-02 19:27:54 +0300

    regex

--- regular_expressions/rbbbr.py
+++ regular_expressions/rbbbr.py
@@ -2,8 +2,8 @@ import re
 
 text = input('Input your text:\n')
 pattern = r'[Rr]{1}[Bb]+r{1}'
-match = re.search(pattern, text)
+match = re.findall(pattern, text)
 if match:
-    print(match.group())
+    print(match)
 else:
     print('Pattern not found')
